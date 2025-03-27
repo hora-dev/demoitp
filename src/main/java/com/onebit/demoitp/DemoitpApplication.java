d6fa8b569f5c610165eb94b65566bb4579a33a7d
horadev <horacio@megawebs.com.ar>
2025-03-27 20:58:38 -0300

    Initial commit

---
+++ src/main/java/com/onebit/demoitp/DemoitpApplication.java
@@ -0,0 +1,13 @@
+package com.onebit.demoitp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoitpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoitpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onebit/demoitp/DemoitpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebit.demoitp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoitpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
